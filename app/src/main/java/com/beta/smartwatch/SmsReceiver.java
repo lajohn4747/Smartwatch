d935346f6d59aa3d54ca40c4a347bd0e73895600
mtmcgraw <mtmcgraw@mit.edu>
2016-02-01 03:47:20 -0500

    added contacts to app

--- app/src/main/java/com/beta/smartwatch/SmsReceiver.java
+++ app/src/main/java/com/beta/smartwatch/SmsReceiver.java
@@ -1,9 +1,13 @@
 package com.beta.smartwatch;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
@@ -38,7 +42,8 @@ public class SmsReceiver extends BroadcastReceiver {
 
                     Log.d("SmsReceiver", "senderNum: " + senderNum + "; message: " + message);
                     MainActivity mainActivity = MainActivity.getInstance();
-                    mainActivity.updateLastText("From: "+senderNum+"\n"+"Message: " + message + "\n");
+                    String from = getContactName(mainActivity.getApplicationContext(), senderNum);
+                    mainActivity.updateLastText("From: "+from+"\n"+"Message: " + message + "\n");
 
                 } // end for loop
             } // bundle is null
@@ -47,5 +52,24 @@ public class SmsReceiver extends BroadcastReceiver {
             Log.e("SmsReceiver", "Exception smsReceiver" + e);
 
         }
+    }
+
+    public static String getContactName(Context context, String phoneNumber) {
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        String contactName = null;
+        if(cursor.moveToFirst()) {
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+        }
+
+        if(cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+
+        return contactName;
     }
 }
